e11649c78878b169df2c155c34c7bd54cce94dd9
Arthur Minduca <arthur.minduca@gmail.com>
2024-12-12 21:02:30 -0500

    prompt for confirmation before applying the changes

--- src/alesis.py
+++ src/alesis.py
@@ -1,6 +1,11 @@
+from __future__ import annotations
+
 import pathlib
+from dataclasses import dataclass
+from typing import Any
 
 import soundfile
+from numpy import dtype, float64, ndarray
 
 from src import files
 
@@ -10,50 +15,90 @@ AUDIO_SUPPORTED_SUBTYPES = {"PCM_S8", "PCM_U8", "PCM_16", AUDIO_TARGET_SUBTYPE}
 AUDIO_MAX_SAMPLE_RATE = 48000
 
 
-class AlesisAudioConversionHandler(files.FileHandler):
+@dataclass(frozen=True)
+class AudioData:
+    """Audio Data"""
+
+    info: soundfile._SoundFileInfo
+    """Sound file info"""
+
+    data: ndarray[Any, dtype[float64]] | Any
+    """Binary"""
+
+    current_samplerate: int
+    """Current sample rate"""
+
+    @property
+    def target_samplerate(self):
+        """Desired sample rate"""
+        return min(self.current_samplerate, AUDIO_MAX_SAMPLE_RATE)
+
+    @property
+    def needs_rescaling(self):
+        """Returns True if audio needs to be converted to be supported in Alesis Strike Multipad. Otherwise, returns False"""
+        return (
+            self.info.subtype not in AUDIO_SUPPORTED_SUBTYPES
+            or self.target_samplerate != self.current_samplerate
+        )
+
+
+class AlesisAudioConversionHandler(files.FileCmdGetter):
     """
     The Alesis Strike Multipad natively supports 16-bit/44.1kHz samples.
         See https://support.alesis.com/en/support/solutions/articles/69000824218-alesis-strike-multipad-what-file-format-should-i-use-for-my-samples-
     """
 
-    def handle(self, file: pathlib.Path):
+    @dataclass(frozen=True)
+    class CmdConvertAudioFile(files.Cmd):
+        """Command to convert or rescale an audio file, if needed, to a format supported by the Alesis Strike Multipad"""
+
+        file: pathlib.Path
+        """Corresponding File"""
+
+        audio_data: AudioData
+        """Audio Data"""
+
+        @property
+        def operation_description(self) -> str:
+            return f"Convert audio : {self.audio_data.info.subtype}|{self.audio_data.current_samplerate} -> {AUDIO_TARGET_SUBTYPE}|{self.audio_data.target_samplerate} ({self.file.name})"
+
+        def execute(self):
+            """Rewrites the audio file, applying the conversion"""
+            soundfile.write(
+                str(self.file),
+                self.audio_data.data,
+                self.audio_data.target_samplerate,
+                subtype=AUDIO_TARGET_SUBTYPE,
+            )
+
+    def get_command(self, file: pathlib.Path):
         """
         Converts or rescale the audio file, if needed, to a format supported by the Alesis Strike Multipad
         """
 
-        info = soundfile.info(str(file))
-
         # Read the original audio file
-        data, samplerate = soundfile.read(str(file))
+        data, current_samplerate = soundfile.read(str(file))
 
-        target_samplerate = min(samplerate, AUDIO_MAX_SAMPLE_RATE)
-
-        audio_needs_rescaling = (
-            info.subtype not in AUDIO_SUPPORTED_SUBTYPES
-            or target_samplerate != samplerate
+        audio_data = AudioData(
+            info=soundfile.info(str(file)),
+            data=data,
+            current_samplerate=current_samplerate,
         )
 
-        if audio_needs_rescaling:
-            # Rewrite the audio file
-            print(
-                f"{info.subtype}|{samplerate} -> {AUDIO_TARGET_SUBTYPE}|{target_samplerate} ({file.name})"
+        if audio_data.needs_rescaling:
+            return AlesisAudioConversionHandler.CmdConvertAudioFile(
+                file=file, audio_data=audio_data
             )
-            soundfile.write(
-                str(file), data, target_samplerate, subtype=AUDIO_TARGET_SUBTYPE
-            )
-            return True
-        return False
 
-    def close(self, count_files_changed: int) -> None:
-        """Function called at the end of the processing"""
+        return None
 
 
-def normalize_audio_filenames(root_dir: str):
+def normalize_audio_filenames(root_dir: str, formatter: files.RenameFileCmdGetter):
     """Normalizes audio files names"""
     files.update_files(
         root_dir,
         predicate=_is_supported_audio_file,
-        handler=files.TitleCaseFileFormatter(),
+        cmd_getter=formatter,
     )
 
 
@@ -62,7 +107,7 @@ def convert_audio_files(root_dir: str):
     files.update_files(
         root_dir,
         predicate=_is_supported_audio_file,
-        handler=AlesisAudioConversionHandler(),
+        cmd_getter=AlesisAudioConversionHandler(),
     )
 
 
--- src/files.py
+++ src/files.py
@@ -2,53 +2,142 @@ import os
 import pathlib
 import re
 from abc import ABC, abstractmethod
+from dataclasses import dataclass
 from typing import Callable
 
 
-class FileHandler(ABC):
+class Cmd(ABC):
+    """Command"""
+
+    @property
+    @abstractmethod
+    def operation_description(self) -> str:
+        """User-level description of the operation performed by this command execution"""
+
+    @abstractmethod
+    def execute(self) -> None:
+        """Executes the command"""
+
+
+class FileCmdGetter(ABC):
     """Perform actions on files"""
 
     @abstractmethod
-    def handle(self, file: pathlib.Path) -> bool:
-        """Returns True if any change was applies to the file. Otherwise, returns False"""
-
-    @abstractmethod
-    def close(self, count_files_changed: int) -> None:
-        """Function called at the end of the processing"""
+    def get_command(self, file: pathlib.Path) -> Cmd | None:
+        """Returns a command to be executed against this file. Returns None if there is no operation to be performed"""
 
 
-class TitleCaseFileFormatter(FileHandler):
-    """Sanitizes and normalizes the file name"""
+class RenameFileCmdGetter(FileCmdGetter):
+    """Formats file names"""
 
-    def handle(self, file: pathlib.Path) -> bool:
-        file_new = file.with_name(self._normalize_filename(file))
+    @dataclass(frozen=True)
+    class CmdRenameFile(Cmd):
+        """Command to rename a file"""
+
+        file: pathlib.Path
+        """Corresponding File"""
+
+        file_new: pathlib.Path
+        """New name"""
+
+        @property
+        def operation_description(self) -> str:
+            return f"Rename '{self.file}' -> '{self.file_new.name}'"
+
+        def execute(self):
+            """Applies the rename"""
+            os.rename(str(self.file), str(self.file_new))
+
+    def __init__(self, chain: list[Callable[[str], str]]) -> None:
+        self._chain = chain
+
+    def get_command(self, file: pathlib.Path) -> CmdRenameFile | None:
+        file_new = file.with_name(file.name)
+        for fn in self._chain:
+            file_new = file.with_name(fn(file_new))
 
         if file_new.name != file.name:
-            print(f"{file.name} -> {file_new.name}")
-            os.rename(str(file), str(file_new))
-            return True
-        return False
+            return RenameFileCmdGetter.CmdRenameFile(file=file, file_new=file_new)
+        return None
 
-    def close(self, count_files_changed: int):
-        print(f"{count_files_changed} files renamed")
 
-    def _normalize_filename(self, file: pathlib.Path):
-        """Sanitizes and normalizes the file name"""
+class TitleCaseSanitizerFormatter:
+    """Sanitizes and normalizes the file name"""
+
+    def __call__(self, file: pathlib.Path) -> str:
+        """Converts to Title Case and replaces special characters with white spaces"""
         stem = re.sub("[^0-9a-zA-Z]+", " ", file.stem).strip().title()
         suffix = file.suffix.lower().strip() if file.suffix else ""
         return stem + suffix
 
 
+class RegexReplaceFileNameFormatter:
+    """Renames the file that matches regex patterns"""
+
+    def __init__(self, patterns: dict[re.Pattern, str]) -> None:
+        self._patterns = patterns
+
+    def __call__(self, file: pathlib.Path) -> str:
+        stem_new = next(
+            (
+                pattern.sub(stem_re, file.name)
+                for pattern, stem_re in self._patterns.items()
+                if pattern.search(file.name)
+            ),
+            None,
+        )
+
+        counter = 0
+
+        def bump_file_name():
+            nonlocal counter
+            counter += 1
+            stem = stem_new.format(count=counter)
+            suffix = file.suffix.lower().strip() if file.suffix else ""
+            name = stem if stem.endswith(suffix) else (stem + suffix)
+            return file.with_name(name)
+
+        file_new: pathlib.Path | None = None
+        if stem_new:
+            file_new = bump_file_name()
+            while file_new.exists():
+                file_new = bump_file_name()
+
+        return (file_new or file).name
+
+
 def update_files(
-    root_dir: str, predicate: Callable[[pathlib.Path], bool], handler: FileHandler
+    root_dir: str, predicate: Callable[[pathlib.Path], bool], cmd_getter: FileCmdGetter
 ):
     """Iterates files recursively and apply the handler"""
-    count_files_changed = 0
+    commands: list[Cmd] = []
 
     for root, _, files in os.walk(root_dir):
         for file_path in files:
             file = pathlib.Path(os.path.join(root, file_path))
-            if predicate(file) and handler.handle(file):
-                count_files_changed += 1
+            if predicate(file):
+                cmd = cmd_getter.get_command(file)
+                if cmd:
+                    commands.append(cmd)
 
-    handler.close(count_files_changed)
+    if commands:
+        print("The following operations will be performed : ")
+
+        for cmd in commands:
+            print(cmd.operation_description)
+
+        answer = input(
+            "The operation is not reversible. Do you wish to continue ? (y/N): "
+        )
+
+        if answer.lower().strip() == "y":
+            for cmd in commands:
+                cmd.execute()
+                print(f"DONE : {cmd.operation_description}")
+
+            print(f"{len(commands)} operations was performed with success.")
+
+        else:
+            print("The operation was aborted")
+    else:
+        print("There is no operation to be performed")
